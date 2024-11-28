0cf404f3cae122d323c6dfdcf7456cc1b58b097a
calvinsyy <dy_calvin@hotmail.my>
2024-11-27 20:47:31 -0500

    A

--- src/utils/populateProjects.ts
+++ src/utils/populateProjects.ts
@@ -69,15 +69,15 @@ export const populateProjects = (
     techonolgies.innerHTML = `${project.technologies}`;
     proj.appendChild(techonolgies);
 
-    // Repository Link
-    if (project.repository.length > 0) {
-      const repoLink = document.createElement('a');
-      repoLink.classList.add('repo-link');
-      repoLink.innerHTML = 'GitHub';
-      repoLink.href = `${project.repository}`;
-      repoLink.target = '_blank';
-      proj.appendChild(repoLink);
-    }
+    // // Repository Link
+if (project.repository && typeof project.repository === 'string' && project.repository.length > 0) {
+  const repoLink = document.createElement('a');
+  repoLink.classList.add('repo-link');
+  repoLink.innerHTML = 'GitHub';
+  repoLink.href = project.repository;  // Safely access repository
+  repoLink.target = '_blank';
+  proj.appendChild(repoLink);
+}
 
     parent.appendChild(proj);
   }
